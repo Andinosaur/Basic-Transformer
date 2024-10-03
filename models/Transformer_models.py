0cace189a9a462fe5ec7aba4ed0e0a1c403f8742
Andinosaur <1850220763@qq.com>
2024-10-03 10:59:47 +0800

    model code continuation

--- models/Transformer_models.py
+++ models/Transformer_models.py
@@ -1,4 +1,6 @@
+import copy
 from torch import log_softmax
+import torch
 import torch.nn as nn
 
 class EncodeDecoder(nn.Module):
@@ -19,6 +21,7 @@ class EncodeDecoder(nn.Module):
     def decoder(self, memory, tgt, src_mask, tgt_mask):
         return self.decoder(memory, tgt, src_mask, tgt_mask)
     
+
 class Generator(nn.Module):
     def __init__(self, d_model, vocab):
         super(Generator, self).__init__()
@@ -26,3 +29,52 @@ class Generator(nn.Module):
 
     def forward(self, x):
         return log_softmax(nn.linear(x), dim=-1)
+    
+
+def clone(models, N):
+    return nn.ModuleList([copy.deepcopy(models)for _ in range(N)])
+
+
+class Encoder(nn.Module):
+    def __init__(self, layer, N):
+        super(Encoder, self).__init__()
+        self.layer = clone(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, mask):
+        x = self.layer(x, mask)
+        return self.norm(x)
+    
+
+class LayerNorm(nn.Module):
+    def __init__(self, feature, eps= 1e-6):
+        super(LayerNorm, self).__init__()
+        self.a_c = nn.parameter(torch.ones(feature))
+        self.b_c = nn.parameter(torch.zeros(feature))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim= True)
+        std = x.std(-1, keepdim= True)
+        return self.a_c * (x - mean) / (std + self.eps) + self.b_c
+
+
+class SublayerConnection(nn.Module):
+    def __init__(self, size):
+        super(SublayerConnection, self).__init__()
+        self.norm = LayerNorm(size)
+        
+    def forward(self, x, sublayer):
+        return x + sublayer(self.norm(x))
+    
+
+class EncoderLayer(nn.Module):
+    def __init__(self, atten_layer, feed_words, size):
+        super(EncoderLayer, self).__init__()
+        self.atten_layer = atten_layer
+        self.feed_words = feed_words
+        self.norm = clone(SublayerConnection(size), 2)
+
+    def forward(self, x):
+        x = self.norm[0](self.atten_layer(x))
+        return self.norm[1](self.feed_words(x))
